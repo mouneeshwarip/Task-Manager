5f411dc802752432e043df5bd29f4d77f7883023
Mouneeshwari Panneerselvam <mouneeshwari.panneerselvam@gmail.com>
2024-07-15 14:45:06 +0000

    migrate schema to postgres db using docker

--- env.py
+++ env.py
@@ -2,7 +2,8 @@ import os
 
 os.environ.setdefault("IP","0.0.0.0.")
 os.environ.setdefault("PORT","5000")
-os.environ.setdefault("SECRET_Key","any_secret_key")
+os.environ.setdefault("SECRET_KEY","any_secret_key")
 os.environ.setdefault("DEBUG","True")
 os.environ.setdefault("DEVELOPMENT","True")
-os.environ.setdefault("DB_URL","postgresql:///taskmanager")
+#os.environ.setdefault("DB_URL","postgresql:///taskmanager")
+os.environ.setdefault("DB_URL", "postgresql://taskuser:taskpassword@localhost:5433/taskmanager")
